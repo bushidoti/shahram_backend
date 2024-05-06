f289e3ba7e32b7dbc9140f9e1410cd0f9e3c58f0
Sajjad <titanxl79@gmail.com>
2024-05-06 18:15:02 +0330

    add color picker

--- panel/models.py
+++ panel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from colorfield.fields import ColorField
 
 
 class Panel(models.Model):
@@ -8,6 +9,7 @@ class Panel(models.Model):
     youtube = models.TextField("لینک یوتیوب", blank=True, null=True)
     shazam = models.TextField("لینک شازم", blank=True, null=True)
     insta = models.TextField("لینک اینستاگرام", blank=True, null=True)
+    color = ColorField(default='#FF0000')
     pic = models.FileField("عکس پروفایل", upload_to='static_media/media/pic/', blank=True, null=True)
     biopic = models.FileField("عکس بیوگرافی", upload_to='static_media/media/pic/', blank=True, null=True)
     carousel1 = models.FileField("عکس اسلاید 1", upload_to='static_media/media/pic/', blank=True, null=True)
--- shahram_backend/settings.py
+++ shahram_backend/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'File',
     'selfie',
     'panel',
+    'colorfield',
     'django_filters',
 ]
 
